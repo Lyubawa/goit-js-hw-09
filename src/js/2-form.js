5e3ec8704296116591e69b733a6d60d9c0b54666
Lyubawa <lyubawa81@ukr.net>
2023-12-22 09:21:08 +0200

    js-hw-9-1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,25 +5,22 @@ const textareaMessage = feedbackForm.querySelector("textarea");
 const inputEmail = feedbackForm.querySelector("email");
 const localStorageKey = "feedback-form-state";
 
-const userForm = {
-    email: "",
-    message: "",
-  };
-
-if (localStorage.getItem(localStorageKey)) {
-    const initialFormData = JSON.parse(localStorage.getItem(localStorageKey));
-    inputEmail.value = initialFormData.email;
-    textareaMessage.value = initialFormData.message;
-    userForm.email = initialFormData.email;
-    userForm.message = initialFormData.message;
-};
-
-
+try {
+    const initialFormData = JSON.parse(localStorage.getItem(feedbackForm));
+    Array.from(form.elements).forEach(element => {
+        const valueStorage = initialFormData[element.name];
+        if (valueStorage) {
+            element.value = valueStorage;
+}
+    })
+} catch (error) {
+    console.error("PARSE ERROR");
+}
 feedbackForm.addEventListener("input", event => {
     const formData = new FormData(feedbackForm);
+    const formObject = {};
         formData.forEach((value, key) => {
-            userForm.email[key] = value.trim();
-            userForm.message[key] = value.trim();
+            formObject[key] = value.trim();
     })
     localStorage.setItem(localStorageKey, JSON.stringify(formObject));
     console.log(formObject);
@@ -35,3 +32,19 @@ feedbackForm.addEventListener("submit", (event) => {
     feedbackForm.reset();
     console.log(initialFormData);
 })
+
+
+
+
+// const userForm = {
+//     email: "",
+//     message: "",
+//   };
+
+// if (localStorage.getItem(localStorageKey)) {
+//     const initialFormData = JSON.parse(localStorage.getItem(localStorageKey));
+//     inputEmail.value = initialFormData.email;
+//     textareaMessage.value = initialFormData.message;
+//     userForm.email = initialFormData.email;
+//     userForm.message = initialFormData.message;
+// };
