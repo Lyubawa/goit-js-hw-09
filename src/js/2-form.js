ea4aad926cfcd4d5012fd7ca78449cfc882a2c52
Lyubawa <lyubawa81@ukr.net>
2023-12-23 17:54:25 +0200

    js-hw-9-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,50 +1,35 @@
 "use strict"
 
 const feedbackForm = document.querySelector(".feedback-form");
-const textareaMessage = feedbackForm.querySelector("textarea");
-const inputEmail = feedbackForm.querySelector("email");
 const localStorageKey = "feedback-form-state";
 
+const { email, message } = feedbackForm.elements;
+
+let initialFormData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+
 try {
-    const initialFormData = JSON.parse(localStorage.getItem(feedbackForm));
-    Array.from(form.elements).forEach(element => {
-        const valueStorage = initialFormData[element.name];
-        if (valueStorage) {
-            element.value = valueStorage;
+    if (initialFormData) {
+        email.value = initialFormData.email || "";
+        message.value = initialFormData.message || "";
+    }
 }
-    })
-} catch (error) {
+catch (error) {
     console.error("PARSE ERROR");
 }
+
 feedbackForm.addEventListener("input", event => {
-    const formData = new FormData(feedbackForm);
-    const formObject = {};
-        formData.forEach((value, key) => {
-            formObject[key] = value.trim();
-    })
-    localStorage.setItem(localStorageKey, JSON.stringify(formObject));
-    console.log(formObject);
-})
+    initialFormData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(initialFormData));
+});
+
 feedbackForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    if (!initialFormData.email || !initialFormData.message) { 
+        alert("Fill all the fields");
+        return;
+    };
     localStorage.removeItem(localStorageKey);
-
     feedbackForm.reset();
     console.log(initialFormData);
-})
-
-
-
-
-// const userForm = {
-//     email: "",
-//     message: "",
-//   };
-
-// if (localStorage.getItem(localStorageKey)) {
-//     const initialFormData = JSON.parse(localStorage.getItem(localStorageKey));
-//     inputEmail.value = initialFormData.email;
-//     textareaMessage.value = initialFormData.message;
-//     userForm.email = initialFormData.email;
-//     userForm.message = initialFormData.message;
-// };
+    initialFormData = {};
+});
